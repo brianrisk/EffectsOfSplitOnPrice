33d25baf58ccb404bc481e6f0c6e28737a98f792
Brian Risk <brian.risk@digitalturbine.com>
2016-03-25 14:24:35 -0400

    master: adding Settings, clearing out Downloader in advance of overhaul

--- src/Stock.java
+++ src/Stock.java
@@ -14,38 +14,10 @@ public class Stock {
 		this.code = code;
 		days = loadStockDays(code);
 	}
-	
-	
-	/**
-	 * Given an Quandl code, this finds the appropriately named file
-	 * in the data/stocks directory and loads it into an ArrayList of
-	 * StockDay objects.  This list is sorted from least recent to
-	 * most recent.
-	 * @param code
-	 * @return
-	 */
-	public  ArrayList<StockDay> loadStockDays(String code) {
-		ArrayList<StockDay> out = new ArrayList<StockDay>();
-		File stockFile = new File("data/stocks/" + code + ".csv");
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(stockFile));
-			String line = br.readLine();
-			//skipping the first line, which is the column names
-			line = br.readLine();
-			while (line != null) {
-				try {
-					out.add(new StockDay(line));
-				} catch (Exception e) {
-					U.p(code);
-				}
-				line = br.readLine();
-			}
-			br.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		Collections.sort(out);
-		return out;
-	}
+
+    public void addDay(StockDay day) {
+        days.add(day);
+    }
+
 
 }
