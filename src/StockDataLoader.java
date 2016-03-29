c0002686aeda86adf41e71736a8451780ea55a75
Brian Risk <brian.risk@digitalturbine.com>
2016-03-29 10:11:49 -0400

    master: filled out StockDataLoader.java

--- src/StockDataLoader.java
+++ src/StockDataLoader.java
@@ -8,6 +8,7 @@ import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Our downloader class.  
@@ -19,8 +20,32 @@ import java.util.ArrayList;
 public class StockDataLoader
 {
 
-
+    /**
+     * Sample EOD CSV line:
+     * A,1999-11-18,45.5,50.0,40.0,44.0,44739900.0,0.0,1.0,29.75972820951089,32.70299803242955,26.16239842594364,28.778638268538003,44739900.0
+     *
+     * @param databaseFile
+     * @return
+     */
     public static ArrayList<Stock> loadStockData(File databaseFile) {
+        HashMap<String, Stock> stocks = new HashMap<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(databaseFile));
+            String line = br.readLine();
+            while (line != null) {
+                int indexOfFirstComma = line.indexOf(',');
+                String stockName = line.substring(0, indexOfFirstComma);
+                String stockData = line.substring(indexOfFirstComma + 1);
+                Stock stock = stocks.get(stockName);
+                if (stock == null) stock = new Stock(stockName);
+                StockDay stockDay = new StockDay(stockData);
+                stock.addDay(stockDay);
+                stocks.put(stockName, stock);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
